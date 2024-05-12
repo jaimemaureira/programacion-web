68b7aff7148589885d561163956d7bb69958432f
jaimemaureira01 <jaime_maureira_a@hotmail.es>
2024-05-12 19:25:36 -0400

    activo maps
    se deja activa la api de mapas
    
    Co-Authored-By: Huru26 <137586334+Huru26@users.noreply.github.com>
    Co-Authored-By: Romy GBen <135152744+romigonzalezb@users.noreply.github.com>

--- Proyecto/maps.js
+++ Proyecto/maps.js
@@ -1,11 +1,19 @@
-function iniciarMap(){
 
-    var coord = {lat:-32.9336482,lng:-71.5260104};
-    var map = new google.maps.Map(document.getElementById('map'),{
-        zoom:10,
-        center: coord
+function initMap() {
+    // Coordenadas para centrar el mapa
+    var myLatLng = {lat: -32.9207824, lng: -71.520513};
 
+    // Crea un nuevo mapa en el elemento div con el id 'map'
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 16// Zoom predeterminado
     });
-    
 
+    // Agrega un marcador en la ubicación especificada
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Mi ubicación'
+    });
 }
+    
