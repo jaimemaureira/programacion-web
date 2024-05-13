e908d6c7d4104aca1bb21e3b72198abb7f84c824
Romy GBen <135152744+romigonzalezb@users.noreply.github.com>
2024-05-13 10:08:36 -0400

    Update validationContact.js
    validación ciudad

--- Proyecto/validationContact.js
+++ Proyecto/validationContact.js
@@ -14,27 +14,39 @@ document.getElementById('inputname2').addEventListener('input', function(event) 
                                                            
 document.getElementById('inputAp').addEventListener('input', function(event) {                         // VALIDACION CAMPO APELLIDO2, formulario contacto
     var Apellido2 = event.target.value.trim(); // Eliminar espacios en blanco al principio y al final
-    var ApPattern2 = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/; // Expresión regular para validar nombres
+    var ApPattern2 = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/; // Expresión regular para validar caracteres
     
     var apellidoError = document.getElementById('apellidoError');
     
-    if (!ApPattern2.test(Apellido2)) { // Si el nombre no coincide con el patrón
+    if (!ApPattern2.test(Apellido2)) { // Si el apellido no coincide con el patrón
         apellidoError.style.display = 'inline'; // Mostrar el mensaje de error
     } else {
-        apellidoError.style.display = 'none'; // Ocultar el mensaje de error si el nombre es válido
+        apellidoError.style.display = 'none'; // Ocultar el mensaje de error si el apellido es válido
     }
 });
 
 document.getElementById('inputDireccion').addEventListener('input', function(event) {                         // VALIDACION CAMPO DIRECCIÓN, formulario contacto
     var inputDireccion = event.target.value.trim(); // Eliminar espacios en blanco al principio y al final
-    var direccion2 = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/; // Expresión regular para validar nombres
+    var direccion2 = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/; // Expresión regular para validar caracteres
     
     var direccionError = document.getElementById('direccionError');
     
-    if (!direccion2.test(inputDireccion)) { // Si el nombre no coincide con el patrón
+    if (!direccion2.test(inputDireccion)) { // Si el direcciión no coincide con el patrón
         direccionError.style.display = 'inline'; // Mostrar el mensaje de error
     } else {
-        direccionError.style.display = 'none'; // Ocultar el mensaje de error si el nombre es válido
+        direccionError.style.display = 'none'; // Ocultar el mensaje de error si la direccion es válido
     }
 });
 
+document.getElementById('inputCuidad').addEventListener('input', function(event) {                         // VALIDACION CAMPO CIUDAD, formulario contacto
+    var inputCuidad = event.target.value.trim(); // Eliminar espacios en blanco al principio y al final
+    var ciudad2 = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/; // Expresión regular para validar caracteres
+    
+    var ciudadError = document.getElementById('ciudadError');
+    
+    if (!ciudad2.test(inputCuidad)) { // Si la CIUDAD no coincide con el patrón
+        ciudadError.style.display = 'inline'; // Mostrar el mensaje de error
+    } else {
+        ciudadError.style.display = 'none'; // Ocultar el mensaje de error si la CIUDAD es válido
+    }
+});
