3450cb6a1b30414ceb61476eadd2087ebf6bbebc
jaimemaureira01 <jaime_maureira_a@hotmail.es>
2024-05-13 02:30:25 -0400

    ventas y arriendos
    se reconfigura la pagina

--- Proyecto/maps.js
+++ Proyecto/maps.js
@@ -1,21 +1,21 @@
 
 //mapa casa mediterranea
 
-function initMap() {
-    // Coordenadas para centrar el mapa
-    var myLatLng = {lat: -32.9207824, lng: -71.520513};
+ //   function initMap() {
+        // Coordenadas para centrar el mapa
+//        var myLatLng = {lat: -32.9207824, lng: -71.520513};
 
-    // Crea un nuevo mapa en el elemento div con el id 'map'
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLng,
-        zoom: 16// Zoom predeterminado para la imagen del mapa
-    });
+        // Crea un nuevo mapa en el elemento div con el id 'map'
+ //       var map = new google.maps.Map(document.getElementById('map'), {
+ //           center: myLatLng,
+ //           zoom: 16// Zoom predeterminado para la imagen del mapa
+ //       });
 
-    // Agrega un marcador en la ubicación especificada
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Mi ubicación'
-    });
-}
+        // Agrega un marcador en la ubicación especificada
+ //       var marker = new google.maps.Marker({
+ //           position: myLatLng,
+  //          map: map,
+  //          title: 'Mi ubicación'
+  //      });
+  //  }
     
