52e4e9c7ef6a03d2ad2168842dc46241c61294fc
jaimemaureira01 <jaime_maureira_a@hotmail.es>
2024-05-12 20:04:51 -0400

    se comenta
    se comenta maps.js

--- Proyecto/maps.js
+++ Proyecto/maps.js
@@ -1,4 +1,6 @@
 
+//mapa casa mediterranea
+
 function initMap() {
     // Coordenadas para centrar el mapa
     var myLatLng = {lat: -32.9207824, lng: -71.520513};
@@ -6,7 +8,7 @@ function initMap() {
     // Crea un nuevo mapa en el elemento div con el id 'map'
     var map = new google.maps.Map(document.getElementById('map'), {
         center: myLatLng,
-        zoom: 16// Zoom predeterminado
+        zoom: 16// Zoom predeterminado para la imagen del mapa
     });
 
     // Agrega un marcador en la ubicación especificada
